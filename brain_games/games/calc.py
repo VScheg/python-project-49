e403e6d4feb977134ea5da0d2fbf22ead4b905f7
Schegolkova <brooding@gmail.com>
2024-09-22 14:48:55 +0300

    rewrote calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,11 +1,11 @@
-from operator import concat, sub, mul
+from operator import add, sub, mul
 import random
 
 
 def calculate(number_1: int, number_2: int, operation: str) -> int:
     """Return result of calculation of two numbers."""
     operations = {
-        "+": concat(number_1, number_2),
+        "+": add(number_1, number_2),
         "-": sub(number_1, number_2),
         "*": mul(number_1, number_2)}
     return operations[operation]
